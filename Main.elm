f75a49c1603fe1c75c67142286eac32fdaec930f
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2016-12-02 09:41:51 +0100

    add and update players

--- Main.elm
+++ Main.elm
@@ -46,6 +46,51 @@ type Msg
     | DeletePlay Play
 
 
+resetInput : Model -> Model
+resetInput model =
+    { model | name = "", playerId = Nothing }
+
+
+save : Model -> Model
+save model =
+    case model.playerId of
+        Just id ->
+            edit model id
+
+        Nothing ->
+            add model
+
+
+add : Model -> Model
+add model =
+    let
+        player =
+            { id = (List.length model.players) + 1
+            , name = model.name
+            , points = 0
+            }
+    in
+        { model | players = player :: model.players }
+            |> resetInput
+
+
+edit : Model -> Int -> Model
+edit model id =
+    let
+        newPlayers =
+            List.map
+                (\player ->
+                    if player.id == id then
+                        { player | name = model.name }
+                    else
+                        player
+                )
+                model.players
+    in
+        { model | players = newPlayers }
+            |> resetInput
+
+
 update : Msg -> Model -> Model
 update msg model =
     case msg of
@@ -55,6 +100,16 @@ update msg model =
         Input name ->
             { model | name = name }
 
+        Save ->
+            if model.name /= "" then
+                save model
+            else
+                model
+
+        Cancel ->
+            model
+                |> resetInput
+
         _ ->
             model
 
@@ -63,8 +118,24 @@ view : Model -> Html Msg
 view model =
     div [ class "scoreboard" ]
         [ h1 [] [ text "Score Keeper" ]
+        , playersList model
         , playerForm model
         , p [] [ toString model |> text ]
+        ]
+
+
+playerRow : Player -> Html Msg
+playerRow player =
+    div []
+        [ button [ type_ "button", onClick (Edit player) ] [ text "Edit" ]
+        , span [] [ text player.name ]
+        ]
+
+
+playersList : Model -> Html Msg
+playersList model =
+    div []
+        [ li [] (List.map playerRow model.players)
         ]
 
 
@@ -72,14 +143,14 @@ playerForm : Model -> Html Msg
 playerForm model =
     Html.form [ onSubmit Save ]
         [ input
-            [ type_ "input"
+            [ type_ "text"
             , placeholder "Add/Edit Player"
             , onInput Input
             , value model.name
             ]
             []
-        , button [ type_ "submit" ] [ text "Save" ]
-        , button [ type_ "button", onClick Cancel ] [ text "Cancel" ]
+        , button [ type_ "submit", disabled (model.name == "") ] [ text "Save" ]
+        , button [ type_ "button", disabled (model.name == ""), onClick Cancel ] [ text "Cancel" ]
         ]
 
 
